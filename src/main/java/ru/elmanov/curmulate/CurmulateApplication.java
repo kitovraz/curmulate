4a40380c37b844da770cd8ffdd6e3127790f5de5
kitovraz <elmanov-mr@yandex.ru>
2023-01-23 16:19:50 +0300

    first commit

---
+++ src/main/java/ru/elmanov/curmulate/CurmulateApplication.java
@@ -0,0 +1,13 @@
+package ru.elmanov.curmulate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurmulateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurmulateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/elmanov/curmulate/CurmulateApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.elmanov.curmulate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurmulateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
